7ebd5a9e8317487cb187e2141d72fcdb2636ddc4
Ryan Birmingham <birm@rbirm.us>
2018-08-15 17:07:52 -0400

    simplify bindaas auth for not slide-level

--- bindaas_auth.js
+++ bindaas_auth.js
@@ -1,3 +1,4 @@
+// WIP - slide and colleciotn level auth for bindaaas
 async function bindaaasCheckAuth(type, path, auth, request){
   if (!(type=="data" || type=="img")){
     return true
@@ -6,15 +7,10 @@ async function bindaaasCheckAuth(type, path, auth, request){
   if (!auth){
     return false
   }
-  let user = auth.split(" ")[1];
-  // TODO need a better method to do this
-  // TODO need better auth checking depending on type
-  let slide = getUrlParam("slide", path)
-  if (!slide){
-    // slide is needed
-    return false
-  }
-  // get current user's collections and slides
+  let user = getUrlParam("user", path) || auth.split(" ")[1];
+  // TODO need a better method to auth user
+
+  // get user
   let url = "ca-data:9099/services/caMicroscope/Authorization/query/getAuth?name=" + user
   options = {
     uri: url,
@@ -23,19 +19,14 @@ async function bindaaasCheckAuth(type, path, auth, request){
     resolveWithFullResponse: true,
     json: true
   }
-  // get the slides and collections the user can see
 
   var auth = await rp(options);
-  // PLEASE handle rejection in caller
 
-  function isInCollectionList(slide, collectionList){
-    return false
-    // not supporting collection level auth yet
-  }
-  if (slide in auth.slides){
+  // if there's such a user, we're ok
+  if (auth.length){
     return true
   } else {
-    return isInCollectionList(slide, auth.collections)
+    return false
   }
 }
 

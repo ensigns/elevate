d539b83ddea8b69002ff7db0ebd5e54e58736ba2
Ryan Birmingham <birm@rbirm.us>
2018-08-15 15:24:28 -0400

    change to check only img or data

--- bindaas_auth.js
+++ bindaas_auth.js
@@ -1,5 +1,5 @@
 async function bindaaasCheckAuth(type, path, auth, request){
-  if (type=="viewer"){
+  if (!(type=="data" || type=="img")){
     return true
   }
   // TODO something to translate auth into user better than literal
@@ -7,6 +7,10 @@ async function bindaaasCheckAuth(type, path, auth, request){
   // TODO need a better method to do this
   // TODO need better auth checking depending on type
   let slide = getUrlParam("slide", path)
+  if (!slide){
+    // slide is needed
+    return false
+  }
   // get current user's collections and slides
   let url = "ca-data:9099/services/caMicroscope/Authorization/query/getAuth?name=" + user
   options = {
